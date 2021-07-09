165e204eaab2566d5e3f8f31f243963e0354cc3f
buggsboony <40001279+buggsboony@users.noreply.github.com>
2021-07-09 10:48:34 -0400

    Update start_server.bat
    Oups, comment removed now

--- start_server.bat
+++ start_server.bat
@@ -7,6 +7,6 @@ REM call ./config/prepare.bat
 set php_path=C:\Users\%USERNAME%\progs\php7.3.11\php.exe
 
 echo "phppath est :"
-REM echo "Lancement du serveur de developpement :  %php_path%"
-REM %php_path% -S localhost:8082
+echo "Lancement du serveur de developpement :  %php_path%"
+%php_path% -S localhost:8082
 
